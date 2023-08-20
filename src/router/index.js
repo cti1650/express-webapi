b70a8b756fa0cabafb59366a316201758a97f5db
cti1650 <cti1650@gmail.com>
2023-08-20 12:12:50 +0000

    feat: add router

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", (_req, res) => {
   res.header("Content-Type", "application/json; charset=utf-8");
   res.send('{ "message": "hello world!" }');
 });
-router.get("/sample", (req, res) => res.send("Hello World!!"));
+router.get("/sample", (_req, res) => res.send("Hello World!!"));
 
-module.exports = router
+app.get("/robots.txt", (_req, res) => {
+  res.type("text/plain");
+  res.send("User-agent: *\nDisallow: /");
+});
+
+app.get("/health", (_req, res) => {
+  res.status(OK).json({ status: OK, text: "Health check ok" });
+});
+
+module.exports = router;
