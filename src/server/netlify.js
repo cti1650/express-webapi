b5b49ce173bd1f76090fd5359e67686a3ae7d397
cti1650 <cti1650@gmail.com>
2023-08-20 12:05:48 +0000

    fix: update path

--- src/server/netlify.js
+++ src/server/netlify.js
@@ -1,6 +1,6 @@
 'use strict';
-const app = require('../../app');
-const router = require('../../router/index');
+const app = require('../app');
+const router = require('../router/index');
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
