f86b557574ff590d432d2399b48af8be29dda5ac
cti1650 <cti1650@gmail.com>
2023-08-20 13:56:19 +0000

    feat: update netlify

--- src/server/netlify.js
+++ src/server/netlify.js
@@ -2,6 +2,6 @@
 const app = require('../app');
 const router = require('../router/index');
 
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/api/', router);  // path must route to lambda
 
 module.exports = app;
