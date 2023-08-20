981edb43974c0c7d2270ec7070e42058d67712db
cti1650 <cti1650@gmail.com>
2023-08-20 11:24:54 +0000

    feat:add vercel server

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(
   })
 );
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 module.exports = app
--- src/server/local.js
+++ src/server/local.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 3000;
 const app = require('../app');
 const router = require('../router/index');
 
-app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', router);
 
 app.listen(port, () => console.log(`Local app listening on http://localhost:${port}`));
