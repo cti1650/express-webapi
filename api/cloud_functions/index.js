c65bdcd7bfbfc5f2166cb0349473ca6093fe3a06
cti1650 <cti1650@gmail.com>
2023-08-25 15:01:46 +0000

    feat: update cloudfunctions

--- api/cloud_functions/index.js
+++ api/cloud_functions/index.js
@@ -3,4 +3,6 @@ const functions = require("@google-cloud/functions-framework");
 
 const app = require("../../src/server/cloudFunctions");
 
-functions.http("express_server", app);
+functions.http("expressServer", app);
+
+exports.expressServer = app
